0b068499f9dfe1068007d2122f2b56c9278c41f1
datma <datma113112111@gmail.com>
2021-03-17 20:24:50 +0700

    first commit...

---
+++ app/src/main/java/com/example/btth_t1_android1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.btth_t1_android1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
